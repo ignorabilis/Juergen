9d986838609b08ce62b7f45de9242e6f01cf95cc
ignorabilis <irina.yaroslavova@ignorabilis.com>
2018-10-25 15:58:42 +0300

    Simple loading animation

--- app/components/Loader.js
+++ app/components/Loader.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Animated, Easing, View, Text } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import CameraText from '../components/CameraText';
 
 export default class Loader extends React.Component {
+    spinValue = new Animated.Value(0);
+    spin = this.spinValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg']
+    });
+    
+    componentDidMount() {
+        Animated.loop(
+            Animated.timing(
+                this.spinValue,
+                {
+                    toValue: 1,
+                    duration: 1000,
+                    easing: Easing.linear,
+                    useNativeDriver: true
+                }
+            )
+        ).start()
+    }
+
     render() {
         return (
-            <View style={{ flex: 1 }}>
-                <Text style={{ color: 'white' }}>loading</Text>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Animated.View style={{
+                    width: 48,
+                    height: 48,
+                    transform: [{ rotate: this.spin }]
+                }}>
+                    <MaterialCommunityIcons
+                        name={`loading`}
+                        size={48}
+                        color='white' />
+                </Animated.View>
+                <CameraText>Loading...</CameraText>
             </View>
         );
     }
