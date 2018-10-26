abc03d594ae41ccd7b7551ef3d5dabd504df4516
ignorabilis <irina.yaroslavova@ignorabilis.com>
2018-10-26 13:16:16 +0300

    Enable landscape

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { YellowBox } from 'react-native';
+import {
+  YellowBox,
+  View
+} from 'react-native';
+
+import Expo from 'expo';
+
 import FSCamera from './app/views/FSCamera';
 
 // ignore warnings about remote debugger while... developing
 YellowBox.ignoreWarnings(['Remote debugger']);
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    // app.json does not seem to be enough for changing the orientation
+    Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.ALL)
+  }
+
   render() {
     return (
-      <FSCamera></FSCamera>
+      <View
+        style={{ flex: 1 }}>
+        <FSCamera></FSCamera>
+      </View>
     );
   }
 }
